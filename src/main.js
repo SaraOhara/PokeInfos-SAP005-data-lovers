11868bc759b8b8cad08ed25afbd7d3df9dca2212
Sara Viana <saravianac@outlook.com>
2020-11-24 07:48:56 -0300

    CODE: JS Lógica de programação inicializada

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { example } from './data.js';
-
 import data from './data/pokemon/pokemon.js';
 
+const pokemons = data.pokemon.slice(0, 10); //arrays
+const cardPokemon = document.getElementById("card-pokemon");
+let cards = "";
+for (var chavePokemon in pokemons) {
+    const pokemonAtual = pokemons[chavePokemon];
+
+    cards += `<div class="pokemon">  <h1 id="nome">${pokemonAtual.size.height}</h1><a href="#"><img src="${pokemonAtual.img}" alt="${pokemonAtual.name}"></a></div>`;
+
+}
+cardPokemon.innerHTML = cards;
