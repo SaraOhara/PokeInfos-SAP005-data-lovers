fd09e4fd0659d447a00df668e61131ba5d65204a
Sara Viana <saravianac@outlook.com>
2020-11-25 13:29:21 -0300

    CODE (html, JS, CSS) Inserido os creditos finais

--- src/data.js
+++ src/data.js
@@ -8,3 +8,4 @@ export const anotherExample = () => {
 
   return 'OMG';
 };
+//Lógica aqui
--- src/main.js
+++ src/main.js
@@ -10,3 +10,15 @@ for (var chavePokemon in pokemons) {
 
 }
 cardPokemon.innerHTML = cards;
+
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+    social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+    social_panel_container.classList.remove('visible')
+});
