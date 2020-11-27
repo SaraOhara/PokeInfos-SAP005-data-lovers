23219f0d760c203b4d55a63fc6057949f70964c7
Sara Viana <saravianac@outlook.com>
2020-11-27 08:18:12 -0300

    CODE HTML/CSS Barra de pesquina inicializada

--- src/main.js
+++ src/main.js
@@ -2,22 +2,24 @@ import data from './data/pokemon/pokemon.js';
 
 const pokemons = data.pokemon.slice(0, 50); //arrays
 const cardPokemon = document.getElementById("card-pokemon");
+
 let cards = "";
 for (var chavePokemon in pokemons) {
     const pokemonAtual = pokemons[chavePokemon];
-
+    const types = pokemonAtual.type;
     cards +=
-        `<li class="card">
-        <img class="card-image" alt="${pokemonAtual.name}" src="https://www.serebii.net/pokemongo/pokemon/${pokemonAtual.num}.png" />
+        `<li class="card ${types[0]}">
+        <img class="card-image " alt="${pokemonAtual.name}" src="https://www.serebii.net/pokemongo/pokemon/${pokemonAtual.num}.png" />
         <h2 class="card-title"> ${pokemonAtual.num}. ${pokemonAtual.name} </h2>
-        <p id="card-subtitle">${pokemonAtual.size.height} | ${pokemonAtual.size.weight} </p>
+        <p class="card-subtitle" id="card-subtitle">${pokemonAtual.size.height} | ${pokemonAtual.size.weight} </p>
+        <p class="card-subtitle2" id="card-subtitle2">${types.join(' | ')} </p>
+
     </li>`;
 
-
+    console.log(cards)
 }
 cardPokemon.innerHTML = cards;
 
-console.log(cards)
 
 
-    ;
+
