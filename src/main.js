9708dffbaa298d35d7bdc7db90e1cdb1ceccaedb
Sara Viana <saravianac@outlook.com>
2020-12-04 12:34:25 -0300

    CODE: Logica Pesquisas Iniciadas

--- src/main.js
+++ src/main.js
@@ -1,6 +1,39 @@
 import data from './data/pokemon/pokemon.js';
 import { filterData, orderData } from './data.js';
 
+const pokemons = data.pokemon.slice(0, 102);
+const cardPokemon = document.getElementById("card");
+
+function cardsPokemon() {
+  let cards = "";
+  pokemons.forEach(arr => {
+    const types = arr.type;
+    cards +=
+      `
+      
+      <section class="card ${types[0]}">
+      <div class="flip" >
+     
+            <img class="card-image " alt="${arr.name}" src="https://www.serebii.net/pokemongo/pokemon/${arr.num}.png" />
+            <h2 class="card-title"> ${arr.num}.${arr.name.toLocaleUpperCase().replace("(FEMALE)", "").replace("(MALE)", "")} </h2>
+            <p class="card-subtitle" id="card-subtitle">${arr.size.height} | ${arr.size.weight} <br /> ━━━ <br />${types.join(' | ')} </p>
+            
+            </section >
+            
+        </div>`;
+
+  });
+  cardPokemon.innerHTML = cards;
+}
+cardsPokemon();
+
+
+
+
+
+
+
+
 
 const search = document.getElementById('filter-name');
 search.addEventListener('keyup', function () {
@@ -18,3 +51,4 @@ order.addEventListener('change', function () {
 })
 
 document.getElementById('card').appendChild(card);
+
